a651ff020581507d3cba5358d7758d1168382c8b
lisavdwal <lisa.vanderwal@concepts2go.com>
2024-05-07 13:37:55 +0200

    Section 2: Part 1 - Ep. 11

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,11 @@
+let country = 'The Netherlands';
+let continent = 'Europe';
+let population = '18 million';
+let isIsland = false;
+let language;
+
+console.log(isIsland,population,country,language);
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
